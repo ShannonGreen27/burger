cbf04b8e29185afd7934ff20e872acb5249fde1d
ShannonGreen27 <shannon.jm.green@gmail.com>
2016-12-27 23:42:18 -0600

    minor changes to server and index.hb

--- server.js
+++ server.js
@@ -27,5 +27,7 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+	console.log("express on port " + PORT);
+});
